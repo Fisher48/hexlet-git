1a506bc7d7e09f78cb74416989787028f6866a70
Ivan <156232888+Fisher48@users.noreply.github.com>
2024-01-13 20:00:12 +0300

    Update TEST.java

--- TEST.java
+++ TEST.java
@@ -1,16 +1,17 @@
 public class MainClass {
   static int check(int [] a, int C) {
     int x = -1;
-    for(int i = 0; i < a.length; i++)
-      if(a[i] == C){
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] == C) {
         x = i;
         break;
       }
+    }
       return x;
   }  
-  public static void main(String[] args) {
+   public static void main(String[] args) {
     int[] a = {1,5,8,4,9};
     int C = 8;
     System.out.print(check(a, C));
-  }
+   }
 }
